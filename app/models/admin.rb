cb9a3e15f38e46e806e78599dbcb7cde8904de01
Yash Shah <yash.shah@joshsoftware.com>
2025-02-27 15:04:02 +0530

    rubocop

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,5 @@
 class Admin < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, 
-         :rememberable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
